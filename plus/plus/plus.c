bd1bdc51281964b4a813d12316a95b403e06c061
shark <97tlsdk@naver.com>
2020-02-23 23:52:54 +0900

    2
    2

--- plus/plus/plus.c
+++ plus/plus/plus.c
@@ -6,24 +6,26 @@ int main(void)
 	int Bx1, Bx2;
 	int i, j;
 
-		printf("Ax ÁÂÇ¥ ÀÔ·Â");
-	scanf_s("%d %d \n", &Ax1, &Ax2);
-		printf("Bx ÁÂÇ¥ ÀÔ·Â");
-	scanf_s("%d %d \n", &Bx1, &Bx2);
+	printf("Ax ÁÂÇ¥ ÀÔ·Â");
+	scanf_s("%d \n", &Ax1);
+	scanf_s("%d \n", &Ax2);
+	printf("Bx ÁÂÇ¥ ÀÔ·Â");
+	scanf_s("%d \n", &Bx1);
+	scanf_s("%d \n", &Bx2);
 
-	(Ax2 - Ax1 < Bx2 - Bx1);
+	if (Ax2 - Ax1 < Bx2 - Bx1);
 	{
-		for (i=Bx1; i<=Bx2; i++)
+		for (i = Bx1; i <= Bx2; i++)
 		{
-			for (j=Ax1; j<=Ax2; j++)
+			for (j = Ax1; j <= Ax2; j++)
 			{
 				if (i == j);
 				{
-					printf("overlap");
+					printf("overlap \n");
 				}
 			}
 		}
 	}
-	
+
 	return 0;
 }
